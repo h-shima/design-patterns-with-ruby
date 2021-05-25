4e31e8641980c0d2c4720d8ab74d778219f711c1
Oshima Hiroki <hiroki.oshima@MOB20007.local>
2021-05-25 15:23:00 +0900

    Impl interator pattern

---
+++ iterator.rb
@@ -0,0 +1,28 @@
+class ArrayIterator
+  def initialize(array)
+    @array = array
+    @index = 0
+  end
+
+  def has_next?
+    @index < @array.length
+  end
+
+  def item
+    @array[@index]
+  end
+
+  def next_item
+    value = @array[@index]
+    @index += 1
+    value
+  end
+end
+
+array = %w(red green blue)
+
+i = ArrayIterator.new(array)
+
+while i.has_next?
+  puts "item: #{i.next_item}"
+end
